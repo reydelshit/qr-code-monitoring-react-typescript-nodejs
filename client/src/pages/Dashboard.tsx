1ec255edeb8045acb7babdfb747b9323d946ced2
Reydel Ocon <reydel321@gmail.com>
2024-09-05 23:35:56 +0800

    added graph

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,8 +1,99 @@
 import { MailCheck, ScrollText, UserCog } from 'lucide-react';
+import moment from 'moment';
+import useSWR from 'swr';
+
+import { Bar, BarChart, CartesianGrid, XAxis } from 'recharts';
+
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from '@/components/ui/chart';
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import PaginationTemplate from '@/components/Pagination';
+import usePagination from '@/hooks/usePagination';
+
+const chartData = [
+  { month: 'January', desktop: 186, mobile: 80, color: '#0D7C66' },
+  { month: 'February', desktop: 305, mobile: 200, color: '#821131' },
+  { month: 'March', desktop: 237, mobile: 120, color: '#2563eb' },
+  { month: 'April', desktop: 73, mobile: 190, color: '#7c3aed' },
+  { month: 'May', desktop: 209, mobile: 130, color: '#db2777' },
+  { month: 'June', desktop: 214, mobile: 140, color: '#ea580c' },
+  { month: 'July', desktop: 189, mobile: 100, color: '#65a30d' },
+  { month: 'August', desktop: 239, mobile: 120, color: '#0891b2' },
+  { month: 'September', desktop: 349, mobile: 200, color: '#4f46e5' },
+  { month: 'October', desktop: 278, mobile: 190, color: '#b91c1c' },
+  { month: 'November', desktop: 298, mobile: 210, color: '#854d0e' },
+  { month: 'December', desktop: 259, mobile: 220, color: '#115e59' },
+];
+
+const CustomBar = (props: any) => {
+  const { x, y, width, height, fill, payload } = props;
+  return (
+    <rect
+      x={x}
+      y={y}
+      width={width}
+      height={height}
+      fill={payload.color || fill}
+    />
+  );
+};
 
 const Dashboard = () => {
+  const chartConfig = {
+    desktop: {
+      label: 'Desktop',
+      color: '#CD5C08',
+    },
+  } satisfies ChartConfig;
+
+  const fetcher = async (url: string) => {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  };
+
+  const { data: messages = [] } = useSWR(
+    `${import.meta.env.VITE_SERVER_LINK}/messages`,
+    fetcher,
+  );
+
+  const { data: attendance = [] } = useSWR(
+    `${import.meta.env.VITE_SERVER_LINK}/attendance`,
+    fetcher,
+  );
+
+  const { data: students = [] } = useSWR(
+    `${import.meta.env.VITE_SERVER_LINK}/student`,
+    fetcher,
+  );
+
+  const filteredAttendance = attendance
+    ?.filter((today: any) => moment(today.timeIn).isSame(moment(), 'day'))
+    .sort((a: any, b: any) => moment(b.timeIn).diff(moment(a.timeIn)));
+
+  const { currentItems, totalPages, currentPage, handlePageChange } =
+    usePagination({
+      itemsPerPage: 8,
+      data: filteredAttendance,
+    });
+
   return (
-    <div className="w-full">
+    <div className="h-full w-full">
       <div className="my-4">
         <h1 className="text-6xl font-bold">Dashboard</h1>
         {/* <p className="font-semibold">Welcome to the dashboard</p> */}
@@ -12,7 +103,9 @@ const Dashboard = () => {
         <div className="mt-[2rem] grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
           <div className="flex h-[10rem] items-center justify-center gap-10 rounded-3xl border-[1px] shadow-md">
             <div>
-              <span className="block text-6xl font-bold">20</span>
+              <span className="block text-6xl font-bold">
+                {students.length}
+              </span>
               <p className="font-semibold">Total Students</p>
             </div>
             <UserCog color="orange" size={80} className="mr-2" />
@@ -20,7 +113,13 @@ const Dashboard = () => {
 
           <div className="flex h-[10rem] items-center justify-center gap-10 rounded-3xl border-[1px] shadow-md">
             <div>
-              <span className="block text-6xl font-bold">99</span>
+              <span className="block text-6xl font-bold">
+                {
+                  attendance?.filter((today: any) =>
+                    moment(today.timeIn).isSame(moment(), 'day'),
+                  ).length
+                }
+              </span>
               <p className="font-semibold">Today's Attendance</p>
             </div>
             <ScrollText color="orange" size={80} className="mr-2" />
@@ -28,11 +127,79 @@ const Dashboard = () => {
 
           <div className="flex h-[10rem] items-center justify-center gap-10 rounded-3xl border-[1px] shadow-md">
             <div>
-              <span className="block text-6xl font-bold">69</span>
+              <span className="block text-6xl font-bold">
+                {messages.length}
+              </span>
               <p className="font-semibold">Total Messages</p>
             </div>
             <MailCheck color="orange" size={80} className="mr-2" />
           </div>
+        </div>
+      </div>
+      <div className="flex h-[450px] items-end gap-2">
+        <ChartContainer
+          config={chartConfig}
+          className="mt-[2rem] max-h-[500px] w-[60%]"
+        >
+          <BarChart accessibilityLayer data={chartData}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+
+            <Bar
+              dataKey="desktop"
+              fill="#000000"
+              radius={4}
+              shape={<CustomBar />}
+            />
+          </BarChart>
+        </ChartContainer>
+
+        <div className="flex h-full flex-col py-4">
+          <h1 className="my-4 font-bold">TODAYS ENTRIES</h1>
+          <p className="text-[12px] font-semibold">Only shows 8 per page</p>
+          <Table>
+            <TableCaption>A list of todays attendance.</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead>No.</TableHead>
+                <TableHead className="w-[100px]">Student ID</TableHead>
+                <TableHead>Time In</TableHead>
+                <TableHead>Time Out</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody className="h-[1rem] text-[12px]">
+              {currentItems.map((entry: any, index) => {
+                return (
+                  <TableRow key={index}>
+                    <TableCell>{index + 1}</TableCell>
+                    <TableCell className="text-sm font-semibold">
+                      {entry.student_id_code}
+                    </TableCell>
+                    <TableCell className="text-[12px]">
+                      {moment(entry.timeIn).format('llll')}
+                    </TableCell>
+                    <TableCell>
+                      {entry.timeOut === 'n/a'
+                        ? 'Not yet set'
+                        : moment(entry.timeOut).format('llll')}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+
+          <PaginationTemplate
+            totalPages={totalPages}
+            currentPage={currentPage}
+            handlePageChange={handlePageChange}
+          />
         </div>
       </div>
     </div>
