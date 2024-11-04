bb1a9cb9581326e524dccf7e5651b704a9d60948
Reydel Ocon <reydel321@gmail.com>
2024-11-04 10:11:42 +0800

    sjacje

--- backend/connections/DatabaseConnection.ts
+++ backend/connections/DatabaseConnection.ts
@@ -1,8 +1,9 @@
 import mysql from 'mysql2';
 import fs from 'fs';
 import path from 'path';
+import dotenv from 'dotenv';
 
-
+dotenv.config();
 
 const databaseConnection: mysql.Connection = mysql.createConnection({
     // host: 'localhost',
@@ -10,12 +11,12 @@ const databaseConnection: mysql.Connection = mysql.createConnection({
     // password: '',
     // database: 'qr-code-monitoring',
 
-// s 
-    host: 'mysql-2585ff3d-reydel321-f5a4.h.aivencloud.com',
-    user: 'avnadmin',
-    password: 'AVNS_tCXIZezqSVfOC-pXWWD',
-    database: 'qr-code-monitoring',
-    port: 12163,
+
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: parseInt(process.env.DB_PORT || '3306'),
 
     ssl: {
       ca: fs.readFileSync(path.join(__dirname, 'ca.pem')),
