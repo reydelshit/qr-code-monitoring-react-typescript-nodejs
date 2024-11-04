fe6469d6177ea2320eafb3f60bcbe6852e85e516
Reydel Ocon <reydel321@gmail.com>
2024-11-04 11:41:48 +0800

    damn vercel

--- backend/index.ts
+++ backend/index.ts
@@ -42,7 +42,7 @@ app.listen(PORT, () => {
 app.use((req, res, next) => {
     res.header(
       'Access-Control-Allow-Origin',
-     process.env.VITE_SERVER_LINK || 'http://localhost:5173/',
+     process.env.VITE_SERVER_LINK || 'http://localhost:5173',
     );
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
