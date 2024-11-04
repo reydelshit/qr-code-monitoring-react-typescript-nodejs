1cbb8965030bcd4f542200127ba60739e95d5bbc
Reydel Ocon <reydel321@gmail.com>
2024-11-04 10:09:32 +0800

    oh shesh

--- backend/connections/DatabaseConnection.ts
+++ backend/connections/DatabaseConnection.ts
@@ -1,11 +1,26 @@
-import mysql from 'mysql';
+import mysql from 'mysql2';
+import fs from 'fs';
+import path from 'path';
+
 
 
 const databaseConnection: mysql.Connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
+    // host: 'localhost',
+    // user: 'root',
+    // password: '',
+    // database: 'qr-code-monitoring',
+
+// s 
+    host: 'mysql-2585ff3d-reydel321-f5a4.h.aivencloud.com',
+    user: 'avnadmin',
+    password: 'AVNS_tCXIZezqSVfOC-pXWWD',
     database: 'qr-code-monitoring',
+    port: 12163,
+
+    ssl: {
+      ca: fs.readFileSync(path.join(__dirname, 'ca.pem')),
+      rejectUnauthorized: true,  
+  }
   });
   
   
