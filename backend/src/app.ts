91e9a15b6a32d12ceef12b032f195de35a55348b
Reydel Ocon <reydel321@gmail.com>
2024-11-18 10:23:43 +0800

    shace

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,16 @@ const app: Application = express();
 const PORT = process.env.DB_PORT;
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // Allow requests from your frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add the HTTP methods you need
+    credentials: true, // Allow cookies if required
+  }),
+);
+
 app.use('/api/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api', routes);
