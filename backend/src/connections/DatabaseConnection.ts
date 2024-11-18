4286939ecff33df41f6dba8658784a13d29f4688
Reydel Ocon <reydel321@gmail.com>
2024-11-18 09:44:39 +0800

    shace

--- backend/src/connections/DatabaseConnection.ts
+++ backend/src/connections/DatabaseConnection.ts
@@ -1,24 +1,25 @@
 import dotenv from 'dotenv';
 import mysql from 'mysql2';
-
+import path from 'path';
+import fs from 'fs';
 dotenv.config();
 
 const databaseConnection: mysql.Connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'qr-code-monitoring',
+  // host: 'localhost',
+  // user: 'root',
+  // password: '',
+  // database: 'qr-code-monitoring',
 
-  //   host: process.env.DB_HOST,
-  //   user: process.env.DB_USER,
-  //   password: process.env.DB_PASS,
-  //   database: process.env.DB_NAME,
-  //   port: parseInt(process.env.DB_PORT || '3306'),
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT || '3306'),
 
-  //   ssl: {
-  //     ca: fs.readFileSync(path.join(__dirname, 'ca.pem')),
-  //     rejectUnauthorized: true,
-  // }
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, 'ca.pem')),
+    rejectUnauthorized: true,
+  },
 });
 
 databaseConnection.connect((err) => {
